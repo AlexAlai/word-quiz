9417348ec6d66d0902e48a8153e6e59c3e7e0629
AlexAlai <114865595+AlexAlai@users.noreply.github.com>
2023-04-02 07:26:35 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
+import { englishWords } from "./englishWords.js"
 let imgcookie = document.getElementById("imgcookie")
+let letters = ""
+let rules = document.getElementById("rules")
 let imgnumber = 0
 let title = document.getElementById("title")
 let input_is_so_cute = document.getElementById("input_is_so_cute")
-let secretword = "bluepies"
+let words = ["sun", "cookie", "chocolate", "spider", "mom", "dad", "pizza", "seven","phoenix","smile"]
+words = englishWords
+let secretword = words[Math.floor(Math.random()*words.length)]
 let shifr = document.getElementById("shifr")
 shifr.innerHTML = "*".repeat(secretword.length)
-console.log("Big Fat Man");
+console.log(englishWords);
 let okcookie = document.getElementById("okcookie")
 okcookie.onclick = function (event) {
   event.preventDefault();
+  if (!letters.includes(input_is_so_cute.value)) {
+    letters = (letters + input_is_so_cute.value)
+  }
+  rules.innerHTML = ("used letters: " + letters)
   if (secretword.includes(input_is_so_cute.value)) {
     console.log("correct");
     console.log(input_is_so_cute.value);
-    newshifr = ""
-    for (let i = 0; i < 8; i = i + 1) {
+    let newshifr = ""
+    for (let i = 0; i < secretword.length; i = i + 1) {
       console.log(secretword[i]);
       if (secretword[i] == input_is_so_cute.value) {
         newshifr = newshifr + input_is_so_cute.value
@@ -22,23 +31,36 @@ okcookie.onclick = function (event) {
         newshifr = newshifr + shifr.innerHTML[i]
       }
     }
-     shifr.innerHTML = newshifr
-     if(shifr.innerHTML==secretword){
-       title.innerHTML = "you win"
-      }
+    shifr.innerHTML = newshifr
+    if (shifr.innerHTML == secretword) {
+      title.innerHTML = "you win"
+      okcookie.disabled = true
+    }
 
   }
   else {
-    title.innerHTML = "you lose"
+    title.innerHTML = "hangman"
     console.log("incorrect")
-    imgnumber=imgnumber +1
-    imgcookie.src="hangman"+ imgnumber+".png"
+    imgnumber = imgnumber + 1
+    imgcookie.src = "hangman" + imgnumber + ".png"
+  }
+  if (imgnumber == 6) {
+    okcookie.disabled = true
+    title.innerHTML = "you lose your secret word was "+(secretword)
   }
   input_is_so_cute.select()
 }
 okcandy.onclick = function (event) {
   event.preventDefault();
-  console.log(imgnumber=imgnumber +1)
+  console.log(imgnumber = imgnumber + 1)
+  title.innerHTML = "hangman"
+  imgnumber = 0
+  imgcookie.src = "hangman" + imgnumber + ".png"
+  okcookie.disabled = false
+secretword =  words[Math.floor(Math.random()*words.length)]
+  shifr.innerHTML = "*".repeat(secretword.length)
+  letters=""
+  rules.innerHTML = ("type letter and press ok")
 }
 for (let i = 27; i > 0.5; i = i / 3) {
   console.log(i);
